7664d72ed5eded53cfdecd15141d9f6990115a1b
Peyton Kellogg <pekellogg@hotmail.com>
2022-10-31 08:54:23 -0700

    removed Header component for redundancy & basic Navigation component displaying correctly

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import RoverShowcase from "./features/rovers/rovers-list.component";
 import "./styles/App.css"
+import Navigation from "./features/navigation.component.jsx"
 
 export default class App extends React.Component {
   render () {
     return (
       <div className="App">
-        <h1>Mars Rovers Photos</h1>
+        <Navigation />
+        <h1>Mars Rovers</h1>
         <div>
           <RoverShowcase />
         </div>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/App.css";
 import App from "./App";
-import Header from "./features/header.component.jsx";
 import Navigation from "./features/navigation.component.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App>
-      <Header />
-      <Navigation />
-    </App>
+    <App />
   </React.StrictMode>
 );
