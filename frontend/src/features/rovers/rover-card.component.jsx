39e6f15a920d5802554b032d0cc8b6d6dc5c4f7d
Peyton Kellogg <pekellogg@hotmail.com>
2022-10-31 08:12:32 -0700

    fixed basic initial rovers rendering

--- frontend/src/features/rovers/rover-card.component.jsx
+++ frontend/src/features/rovers/rover-card.component.jsx
@@ -5,6 +5,10 @@ export default class RoverCard extends React.Component {
 	render() {
 		return (
 			<div className="rover-card">
+				<h1>{this.props.name}</h1>
+				<h2>Mission status: {this.props.status}</h2>
+				<h2>Launch date: {this.props.launchDate}</h2>
+				<h2>Landing date: {this.props.landingDate}</h2>
 			</div>
 		);
 	}
--- frontend/src/features/rovers/rovers-list.component.jsx
+++ frontend/src/features/rovers/rovers-list.component.jsx
@@ -5,15 +5,18 @@ import initRovers from "../../data/init-rover-data.component.jsx";
 export default class RoverShowcase extends Component {
 
 	createRoverCards = () => {
-		return initRovers.map((rover, index) =>
-      <RoverCard
-        key={index}
-        name={rover.name}
-        landing_date={rover.landing_date}
-        launch_date={rover.launch_date}
-        status={rover.status}
-      />
-    )
+		return initRovers.map((rover) => {
+      return (
+        <RoverCard
+          key={rover.externalID}
+          name={rover.name}
+          landingDate={rover.landingDate}
+          launchDate={rover.launchDate}
+          status={rover.status}
+        />
+      )
+    });
+    
 	};
 
 	render() {
