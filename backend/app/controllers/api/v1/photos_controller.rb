b5c4914b0bfd6984ed7769ee7d44150e7891ba27
Peyton Kellogg <pekellogg@hotmail.com>
2023-01-27 14:00:24 -0800

    removed line breaks

--- backend/app/controllers/api/v1/photos_controller.rb
+++ backend/app/controllers/api/v1/photos_controller.rb
@@ -5,16 +5,6 @@ class Api::V1::PhotosController < ApplicationController
     render json: @photos
   end
 
-  # def create
-  #   @photo = Photo.create(photo_params)
-  #   if @photo.save
-  #     render json: @photo 
-  #   else
-  #     # client error if not saved; 400 Bad Request
-  #     render json: { status: 400 }
-  #   end
-  # end
-
   def show
     @photo = Photo.find(params[:id])
     if @photo
@@ -24,25 +14,5 @@ class Api::V1::PhotosController < ApplicationController
       render json: { status: 500 }
     end
   end
-
-  # def update
-  #   @photo = Photo.find(params[:id])
-  #   if @photo.update(photo_params)
-  #     render json: @photo
-  #   else
-  #     # client error if no update; 400 Bad Request
-  #     render json: { status: 400 }
-  #   end
-  # end
-
-  # def destroy
-  #   Photo.find(params[:id]).destroy
-  # end
-
-  # private
-
-  # def photo_params
-  #   params.require(:photo)#.permit(:id, :assignees, :status, :deadline, :created_at, :updated_at)
-  # end
   
 end
