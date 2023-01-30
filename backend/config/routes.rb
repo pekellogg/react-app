4d4ee54806076cdc2fee2d174059a670ebb43dd2
Peyton Kellogg <pekellogg@hotmail.com>
2023-01-29 22:22:44 -0800

    added nested routes for comments belonging to rovers with 'shallow nesting' as recommended by Rails guides

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      # previous implementation: testing below
+      # resources :rovers, only: [:index]
+      # resources :comments
 
-      resources :rovers, only: [:index]
+      # testing
+      # https://guides.rubyonrails.org/routing.html#nested-resources
+      # generate the collection actions scoped under the parent (sense of the hierarchy),
+      # but to not nest the member actions.
+      # ... only build routes w/ the minimal amount of information to uniquely identify the resource
+
+      resources :rovers, only: [:index, :show] do
+        resources :comments, only: [:index, :create]
+      end
+      resources :comments, only: [:show, :update, :destroy]
       resources :photos, only: [:index]
-      resources :comments
-
     end
   end
 end
