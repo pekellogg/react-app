47eb4c139b1d5bc94c8f53b17fea586313d5145f
Peyton Kellogg <pekellogg@hotmail.com>
2023-01-29 20:59:00 -0800

    added Card presentational component as styling wrapper

--- frontend/src/ui/NavBar.js
+++ frontend/src/ui/NavBar.js
@@ -1,29 +1,32 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
+import Card from "./Card";
 
 class NavBar extends Component {
 
 	render() {
 		return (
-      <header>
-        <nav className="navigation">
-          <NavLink style={{ marginRight: '10px' }} to="/">
-            Home
-          </NavLink>
-          <NavLink style={{ marginRight: '10px' }} to="/rovers">
-            Rovers
-          </NavLink>
-          <NavLink style={{ marginRight: '10px' }} to="/photos">
-            Photos
-          </NavLink>
-          <NavLink style={{ marginRight: '10px' }} to="/data">
-            Data
-          </NavLink>
-          {/* <NavLink style={{ marginRight: '10px' }} to="/comments">
-            Comments
-          </NavLink> */}
-        </nav>
-      </header>
+      <Card>
+        <header>
+          <nav className="navigation">
+            <NavLink style={{ marginRight: '10px' }} to="/">
+              Home
+            </NavLink>
+            <NavLink style={{ marginRight: '10px' }} to="/rovers">
+              Rovers
+            </NavLink>
+            <NavLink style={{ marginRight: '10px' }} to="/photos">
+              Photos
+            </NavLink>
+            <NavLink style={{ marginRight: '10px' }} to="/data">
+              Data
+            </NavLink>
+            {/* <NavLink style={{ marginRight: '10px' }} to="/comments">
+              Comments
+            </NavLink> */}
+          </nav>
+        </header>
+      </Card>
     );
 	};
 
