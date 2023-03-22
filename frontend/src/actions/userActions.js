e43bf68b17c5f6be4c0165b7088d1d3713b7cc25
Peyton Kellogg <pekellogg@hotmail.com>
2023-03-22 12:17:45 -0700

    removed repetitive comments

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -4,18 +4,10 @@ export const fetchUsers = () => {
       type: "LOADING_USERS",
       loading: true
     });
-    // fetch() returns a Promise 
-    // it resolves to the Response object
     fetch("http://127.0.0.1:3000/api/v1/users")
-      // Promise.then() returns another Promise
-      // it takes 2 args: fulfilled callback, rejected callback
       .then((response) => {
-        // response.json() returns another Promise
-        // it resolves with the result of parsing the response body text as JSON
         return response.json()
       })
-      // Promise.then() returns another Promise
-      // it takes 2 args: fulfilled callback, rejected callback
       .then((users) => {
         dispatch({
           type: "FETCHED_USERS",
@@ -61,24 +53,15 @@ export const createUser = (user) => {
       saveInProgress: true
     });
     console.log(JSON.stringify(user));
-    // fetch() returns a Promise 
-    // it resolves to the Response object
     fetch(`http://127.0.0.1:3000/api/v1/users`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(user)
     })
-      // Promise.then() returns another Promise
-      // it takes 2 args: fulfilled callback, rejected callback
       .then((response) => {
-        // response.json() returns another Promise
-        // it resolves with the result of parsing the response body text as JSON
         return response.json()
       })
-      // Promise.then() returns another Promise
-      // it takes 2 args: fulfilled callback, rejected callback
       .then((data) => {
-        // reminder: all attrs from server not avail here from response.json()
         dispatch({
           type: "USER_SAVED",
           saveInProgress: false,
@@ -89,29 +72,21 @@ export const createUser = (user) => {
   };
 };
 
-
 export const editUser = (user) => {
   return (dispatch) => {
     dispatch({
       type: "EDITING_USER",
       editInProgress: true
     });
-    // fetch() returns a Promise 
-    // it resolves to the Response object
     fetch(`http://127.0.0.1:3000/api/v1/users/${user.id}`, {
       method: "PATCH",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(user)
     })
-      // Promise.then() returns another Promise
-      // it takes 2 args: fulfilled callback, rejected callback
       .then((response) => {
-        // response.json() returns another Promise
-        // it resolves with the result of parsing the response body text as JSON
         return response.json()
       })
       .then((data) => {
-        // reminder: all attrs from server not avail here from response.json()
         dispatch({
           type: "USER_EDITED",
           editInProgress: false,
@@ -128,14 +103,10 @@ export const deleteUser = (userId) => {
       type: "DELETING_USER",
       deleteInProgress: true,
     });
-    // fetch() returns a Promise 
-    // it resolves to the Response object
     fetch(`http://127.0.0.1:3000/api/v1/users/${userId}`, {
       method: "DELETE",
       headers: { "Content-Type": "application/json" },
     })
-      // Promise.then() returns another Promise
-      // it takes 2 args: fulfilled callback, rejected callback
       .then((response) => {
         dispatch({
           type: "USER_DELETED",
