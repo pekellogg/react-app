a54d9655bec602a6123a3d9f6bcabed1aac33a1e
Peyton Kellogg <pekellogg@hotmail.com>
2023-03-22 12:50:03 -0700

    displayed all fields for testing purposes

--- backend/app/serializers/user_serializer.rb
+++ backend/app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :password_digest, :username
+  attributes :id, :email, :password_digest, :username, :created_at, :updated_at
 end
