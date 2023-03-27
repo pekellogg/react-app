12413a2e118c69d98bcf2cd7d93a779000683d9b
Peyton Kellogg <pekellogg@hotmail.com>
2023-03-27 15:42:01 -0700

    added has_many :cameras on Rover for best practices/semantics of assoc. + helper methods

--- backend/app/models/rover.rb
+++ backend/app/models/rover.rb
@@ -2,4 +2,5 @@ class Rover < ApplicationRecord
   self.primary_key = "external_id"
   # has_many :photos
   has_many :comments
+  has_many :cameras
 end
--- backend/app/serializers/rover_serializer.rb
+++ backend/app/serializers/rover_serializer.rb
@@ -1,3 +1,3 @@
 class RoverSerializer < ActiveModel::Serializer
-  attributes :id, :external_id, :comments, :landing_date, :launch_date, :name, :profile_pic, :status, :created_at, :updated_at
+  attributes :id, :external_id, :comments, :landing_date, :launch_date, :name, :profile_pic, :status, :cameras, :created_at, :updated_at
 end
