0d7caee19311435d4de5a219fd5a9b514337985e
Peyton Kellogg <pekellogg@hotmail.com>
2023-03-26 18:15:56 -0700

    added :camera_id for visibility

--- backend/app/serializers/photo_serializer.rb
+++ backend/app/serializers/photo_serializer.rb
@@ -1,3 +1,3 @@
 class PhotoSerializer < ActiveModel::Serializer
-  attributes :id, :external_id, :sol, :source, :rover_id, :created_at, :updated_at
+  attributes :id, :external_id, :sol, :source, :rover_id, :camera_id, :created_at, :updated_at
 end
