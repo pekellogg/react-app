49b47bdc67e2c108c63fb10f68dddbe3f384bf5b
Peyton Kellogg <pekellogg@hotmail.com>
2023-03-29 11:08:38 -0700

    Camera's full name updated to button element + added classname for elements to be filtered; added corrsponding CSS to hide display of elements to be filtered

--- frontend/src/components/cameras/Camera.js
+++ frontend/src/components/cameras/Camera.js
@@ -1,14 +1,16 @@
+import "./Camera.css";
+
 const Camera = (props) => {
   return (
-    <div>
-      <p>Name: {props.name}</p>
-      <p>Full name: {props.full_name}</p>
-      <p>ID: {props.id}</p>
-      <p>External ID: {props.external_id}</p>
-      <p>Created at: {props.created_at}</p>
-      <p>Updated at: {props.updated_at}</p>
-      {/* {console.log(props.photos)} */}
-    </div>
+    <>
+      <button>{props.full_name}</button>
+      <p className="Camera">Abbreviated name: {props.name}</p>
+      <p className="Camera">ID: {props.id}</p>
+      <p className="Camera">External ID: {props.external_id}</p>
+      <p className="Camera">Created at: {props.created_at}</p>
+      <p className="Camera">Updated at: {props.updated_at}</p>
+      {console.log(props.photos)}
+    </>
   )
 };
 
