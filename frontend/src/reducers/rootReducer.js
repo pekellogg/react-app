434ea002b9ee24a069335c2badbecae6003189f6
Peyton Kellogg <pekellogg@hotmail.com>
2023-03-29 14:46:35 -0700

    removed photoActions.js + its reducer from rootReducer as no longer necessary

--- frontend/src/reducers/rootReducer.js
+++ frontend/src/reducers/rootReducer.js
@@ -1,12 +1,10 @@
 import camerasReducer from "./camerasReducer";
-import photosReducer from "./photosReducer";
 import roversReducer from "./roversReducer";
 import usersReducer from "./usersReducer";
 import { combineReducers } from "@reduxjs/toolkit";
 
 const rootReducer = combineReducers({
   cameras: camerasReducer,
-  photos: photosReducer,
   rovers: roversReducer,
   users: usersReducer,
 })
