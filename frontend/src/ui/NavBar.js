f525b427799590d406dc1839e335f7392e415ff6
Peyton Kellogg <pekellogg@hotmail.com>
2023-03-29 18:21:32 -0700

    removed code comments

--- frontend/src/ui/NavBar.js
+++ frontend/src/ui/NavBar.js
@@ -11,10 +11,6 @@ class NavBar extends Component {
           <NavLink to="/"> About </NavLink>
           <NavLink to="/rovers"> Rovers </NavLink>
           <NavLink to="/photos"> Photos </NavLink>
-          {/* <NavLink to="/login"> Login </NavLink> */}
-          {/* <NavLink to="/users/new"> Create Account </NavLink> */}
-          {/* Update to reflect dynamic user id */}
-          {/* <NavLink to="/users/:id/edit"> Edit Account </NavLink> */}
         </nav>
       </header>
     );
