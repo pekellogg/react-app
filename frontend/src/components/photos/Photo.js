73325424d9852a0a978a5940e277cf1ef27434b3
Peyton Kellogg <pekellogg@hotmail.com>
2023-03-31 13:53:37 -0700

    removed code comments

--- frontend/src/components/photos/Photo.js
+++ frontend/src/components/photos/Photo.js
@@ -3,11 +3,6 @@ const Photo = (props) => {
     <>
       <h4>Earth date: {props.earth_date}</h4>
       <img alt="Mars" src={props.source} />
-      {/* <p>Sol: {props.sol}</p> */}
-      {/* <p>ID: {props.id}</p> */}
-      {/* <p>External ID: {props.external_id}</p> */}
-      {/* <p>Created at: {props.created_at}</p> */}
-      {/* <p>Updated at: {props.updated_at}</p> */}
     </>
   )
 };
