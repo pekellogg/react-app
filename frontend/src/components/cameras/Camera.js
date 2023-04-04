fcbca88c1eb2366ad4bc206eda04aab31638dcb1
Peyton Kellogg <pekellogg@hotmail.com>
2023-04-03 18:38:36 -0700

    reverted Camera back to presentational component

--- frontend/src/components/cameras/Camera.js
+++ frontend/src/components/cameras/Camera.js
@@ -1,31 +1,16 @@
-import React, { Component } from "react";
 import PhotosContainer from "../photos/PhotosContainer";
 
 import "./Camera.css";
 
-class Camera extends Component {
-
-  state = {
-    active: false,
-  };
-
-  toggleState = () => {
-    this.setState((prevState) => ({ 
-      active: !prevState.active,
-    }))
-  };
-
-  render() {
-    return (
-      <>
-        <button className={this.props.className} id={this.props.id} onClick={this.onClick && this.props.handlesibchange}>
-          { this.props.full_name }
-        </button>
-        <PhotosContainer cameraid={this.props.id} photos={this.props.photos}/>
-      </>
-    );
-  };
-
+const Camera = (props) => {
+  return (
+    <>
+      <button className={props.className} id={props.id} onClick={props.onClick}>
+        { props.full_name }
+      </button>
+      <PhotosContainer cameraid={props.id} photos={props.photos}/>
+    </>
+  );
 };
 
 export default Camera;
