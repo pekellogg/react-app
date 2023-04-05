96bd02554b255fa663c592e3aa2b973de3e2c5c6
Peyton Kellogg <pekellogg@hotmail.com>
2023-04-05 16:29:28 -0700

    removed use of spread operator for state values in lieu of explicit state structure

--- frontend/src/reducers/camerasReducer.js
+++ frontend/src/reducers/camerasReducer.js
@@ -2,18 +2,16 @@ function camerasReducer(state = { all: [], loading: false }, action) {
   switch (action.type) {
     case "LOADING_CAMERAS":
      return { 
-       ...state,
-       loading: action.loading,
-     }
+      all: [],
+      loading: action.loading,
+     };
  
     case "FETCHED_CAMERAS":
      return {
-       ...state,
        all: action.cameras,
-       // add logic to grab camera's photos
        loading: action.loading,
-     }
-    // add a case to update/add photos as there are new available
+     };
+
     default:
      return state;
   }
--- frontend/src/reducers/roversReducer.js
+++ frontend/src/reducers/roversReducer.js
@@ -1,15 +1,13 @@
 function roversReducer(state = { all: [], loading: false }, action) {
  switch (action.type) {
-
    case "LOADING_ROVERS":
     return { 
-      ...state,
+      all: [],
       loading: action.loading,
     };
 
    case "FETCHED_ROVERS":
     return {
-      ...state,
       all: action.rovers,
       loading: action.loading,
     };
