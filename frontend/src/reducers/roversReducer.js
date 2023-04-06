17d52d37a0f0159237dc8563087ef6b8ce4007a6
Peyton Kellogg <pekellogg@hotmail.com>
2023-04-05 18:44:46 -0700

    adjusted spacing for uniformity

--- frontend/src/reducers/roversReducer.js
+++ frontend/src/reducers/roversReducer.js
@@ -1,4 +1,4 @@
-function roversReducer(state = { byId: {}, allIds: [], loading: false }, action) {
+function roversReducer(state = {byId: {}, allIds: [], loading: false}, action) {
   switch (action.type) {
     case "LOADING_ROVERS":
       return {...state, loading: action.loading};
@@ -8,10 +8,7 @@ function roversReducer(state = { byId: {}, allIds: [], loading: false }, action)
       const allIds = [];
       action.payload.map((rover) => {
         const cameras = rover.cameras.map((camera) => `${camera.id}`);
-        byId[`${rover.external_id}`] = {
-          ...rover,
-          cameras
-        };
+        byId[`${rover.external_id}`] = {...rover, cameras};
         allIds.push(`${rover.external_id}`);
       });
       return {byId, allIds, loading: action.loading};
