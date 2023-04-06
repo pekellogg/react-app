a1ff6424bf3719fee8340eb7bf2a6a366e8c94f4
Peyton Kellogg <pekellogg@hotmail.com>
2023-04-05 18:35:16 -0700

    updated tabs + spacing for clarity

--- frontend/src/components/rovers/RoversContainer.js
+++ frontend/src/components/rovers/RoversContainer.js
@@ -1,43 +1,27 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, {Component} from "react";
+import {connect} from "react-redux";
 import RoverCard from "./RoverCard";
 import RoverShow from "./RoverShow";
 import "./RoversContainer.css";
 
 class RoversContainer extends Component {
 
-  state = {
-    "roverid5": {
-      showPage: false, indexPage: true
-    },
-    "roverid6": {
-      showPage: false, indexPage: true
-    },
-    "roverid7": {
-      showPage: false, indexPage: true
-    },
-    "roverid8": {
-      showPage: false, indexPage: true
-    },
+  state = { 
+    "roverid5": {showPage: false, indexPage: true},
+    "roverid6": {showPage: false, indexPage: true},
+    "roverid7": {showPage: false, indexPage: true},
+    "roverid8": {showPage: false, indexPage: true},
   };
 
   initialState = {
-    "roverid5": {
-      showPage: false, indexPage: true
-    },
-    "roverid6": {
-      showPage: false, indexPage: true
-    },
-    "roverid7": {
-      showPage: false, indexPage: true
-    },
-    "roverid8": {
-      showPage: false, indexPage: true
-    },
+    "roverid5": {showPage: false, indexPage: true},
+    "roverid6": {showPage: false, indexPage: true},
+    "roverid7": {showPage: false, indexPage: true},
+    "roverid8": {showPage: false, indexPage: true},
   };
 
   componentDidMount() {
-    { console.log("Component Mounted"); }
+    {console.log("Component Mounted")}
   };
 
   handleClick = (e) => {
@@ -46,18 +30,12 @@ class RoversContainer extends Component {
       if (prevState[target].showPage) {
         return this.initialState;
       } else {
-        const updatedState = { ...prevState };
+        const updatedState = {...prevState};
         for (const rover in updatedState) {
           if (rover !== target) {
-            updatedState[rover] = {
-              showPage: false,
-              indexPage: false,
-            }
+            updatedState[rover] = {showPage: false, indexPage: false}
           } else {
-            updatedState[target] = {
-              showPage: true,
-              indexPage: false,
-            }
+            updatedState[target] = {showPage: true, indexPage: false}
           }
         }
         return updatedState;
@@ -74,12 +52,12 @@ class RoversContainer extends Component {
           <div className="RoverContainer" id={`RoverContainer-${rover.id}`} key={`RoverContainer-${rover.id}`}>
             {[
               <RoverCard
-                { ...rover }
+                {...rover}
                 key={`RoverCard-${rover.id}`}
                 onClick={this.handleClick}
                 roverstate={this.state[`roverid${rover.id}`]}
               />,
-              <RoverShow { ...rover } key={`RoverShow-${this.props.id}`} visible={this.state[`roverid${rover.id}`].showPage}/>
+              <RoverShow {...rover} key={`RoverShow-${this.props.id}`} visible={this.state[`roverid${rover.id}`].showPage}/>
             ]}
           </div>
         ))
@@ -90,12 +68,12 @@ class RoversContainer extends Component {
   render() {
     return (
       <div className="RoversContainer" id="RoversContainer">
-        { this.displayRovers() }
+        {this.displayRovers()}
       </div>
     );
   };
 
 };
 
-const mapStateToProps = (state) => ({ rovers: Object.values(state.rovers.byId) });
+const mapStateToProps = (state) => ({rovers: Object.values(state.rovers.byId)});
 export default connect(mapStateToProps)(RoversContainer);
