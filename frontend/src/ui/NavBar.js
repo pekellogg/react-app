f9c3d671c3681bdc506f120c1cb76963775f965c
Peyton Kellogg <pekellogg@hotmail.com>
2023-04-10 14:25:46 -0700

    adjusted spacing for Application uniformity

--- frontend/src/ui/NavBar.js
+++ frontend/src/ui/NavBar.js
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
-import { NavLink } from "react-router-dom";
+import React, {Component} from "react";
+import {NavLink} from "react-router-dom";
+
 import "./NavBar.css";
 
 class NavBar extends Component {
@@ -8,9 +9,9 @@ class NavBar extends Component {
 		return (
       <header>
         <nav>
-          <NavLink to="/"> About </NavLink>
-          <NavLink to="/rovers"> Rovers </NavLink>
-          <NavLink to="/photos"> Photos </NavLink>
+          <NavLink to="/">About</NavLink>
+          <NavLink to="/rovers">Rovers</NavLink>
+          <NavLink to="/photos">Photos</NavLink>
         </nav>
       </header>
     );
