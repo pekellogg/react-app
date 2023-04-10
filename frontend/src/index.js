3bf2cc18644475c29b88e864bf34a256613cdc81
Peyton Kellogg <pekellogg@hotmail.com>
2023-04-10 14:02:45 -0700

    adjusted spacing for uniformity

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
+import {createRoot} from "react-dom/client";
+import {Provider} from "react-redux";
 import store from "./store/store";
 import App from "./App";
 
@@ -8,7 +8,7 @@ const domNode = document.getElementById("root");
 const root = createRoot(domNode);
 
 root.render(
-  <Provider store={store} >
-    <App />
+  <Provider store={store}>
+    <App/>
   </Provider>,
 );
