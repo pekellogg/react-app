07f553450cf4555d055ef9feaf5063c24c5bcc64
Peyton Kellogg <pekellogg@hotmail.com>
2023-04-10 19:32:36 -0700

    used helper function, capitalizeWord() for CSS display rules by className

--- frontend/src/features/rovers/RoverShow.js
+++ frontend/src/features/rovers/RoverShow.js
@@ -1,10 +1,11 @@
 import CamerasContainer from "../cameras/CamerasContainer";
+import {capitalizeWord} from "../../common/capitalizeWord";
 
 import "./RoverShow.css";
 
 const RoverShow = (props) => {
   return (
-    <div className={`RoverShow${props.visible}`} id={`RoverShow-${props.id}`} onClick={props.onClick}>
+    <div className={`RoverShow${capitalizeWord(props.visible)}`} id={`RoverShow-${props.id}`} onClick={props.onClick}>
       <CamerasContainer {...props} cameraids={props.cameras}/>
     </div>
   );
