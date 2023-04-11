48f9bc3cd8987fc0eb2579274bf8a50f95de6ff7
Peyton Kellogg <pekellogg@hotmail.com>
2023-04-10 19:31:36 -0700

    created general utility/helper function to Capitalize a word

---
+++ frontend/src/common/capitalizeWord.js
@@ -0,0 +1,13 @@
+export const capitalizeWord = (word) => {
+  switch (typeof word) {
+    case "string":
+      return `${word.charAt(0).toUpperCase()}${word.slice(1)}`;
+
+    case "boolean":
+      const booleanToString = word.toString();
+      return `${booleanToString.charAt(0).toUpperCase()}${booleanToString.slice(1)}`;
+    
+    default:
+      return word;
+  }
+};
