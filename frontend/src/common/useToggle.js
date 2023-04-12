664cfeaa3c23d52227f887b2588149af9e7e88db
Peyton Kellogg <pekellogg@hotmail.com>
2023-04-12 15:53:20 -0700

    added new useVisibilityToggle() to alternate CSS visibility property between 'hidden'/'visible'

--- frontend/src/common/useToggle.js
+++ frontend/src/common/useToggle.js
@@ -2,6 +2,14 @@ import {useCallback, useState} from "react";
 
 export const useToggle = (initialState = false) => {
   const [state, setState] = useState(initialState);
-  const toggle = useCallback(() => setState(state => !state), []);
+  const toggle = useCallback(() => setState((prevState) => !prevState), []);
+  return [state, toggle];
+};
+
+export const useVisibilityToggle = (initialState = "hidden") => {
+  const [state, setState] = useState(initialState);
+  const toggle = useCallback(() => setState((prevState) => {
+    return prevState === "hidden" ? "visible" : "hidden"
+  }), []);
   return [state, toggle];
 };
