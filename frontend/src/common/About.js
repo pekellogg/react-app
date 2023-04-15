83b4011cb0007936378b6302391be07ad23024d7
Peyton Kellogg <pekellogg@hotmail.com>
2023-04-14 20:33:58 -0700

    className -> id attribute for better semantics, removed react import as not necessary

--- frontend/src/common/About.js
+++ frontend/src/common/About.js
@@ -1,15 +1,12 @@
-import React from "react";
 import "./About.css";
 
 const About = () => {
-
   return (
-    <div className="about">
+    <div id="about">
       <p>This web application derives data from the Mars Photo API maintained by chrisccerami.</p>
       <p>Explore the <a href="https://github.com/chrisccerami/mars-photo-api">official repository</a>.</p>
     </div>
   );
-
 };
 
 export default About;
