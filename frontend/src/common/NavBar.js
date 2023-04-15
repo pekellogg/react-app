bcbb2b9ec2d40c7f2247d44898935f1e19b16595
Peyton Kellogg <pekellogg@hotmail.com>
2023-04-14 20:47:45 -0700

    changed to function expression from React Class component per React API recommendation

--- frontend/src/common/NavBar.js
+++ frontend/src/common/NavBar.js
@@ -1,22 +1,17 @@
-import React, {Component} from "react";
 import {NavLink} from "react-router-dom";
 
 import "./NavBar.css";
 
-class NavBar extends Component {
-
-	render() {
-		return (
-      <header>
-        <nav>
-          <NavLink to="/">About</NavLink>
-          <NavLink to="/rovers">Rovers</NavLink>
-          <NavLink to="/photos">Photos</NavLink>
-        </nav>
-      </header>
-    );
-	};
-
+const NavBar = () => {
+  return (
+    <header>
+      <nav>
+        <NavLink to="/">About</NavLink>
+        <NavLink to="/rovers">Rovers</NavLink>
+        <NavLink to="/photos">Photos</NavLink>
+      </nav>
+    </header>
+  );
 };
 
 export default NavBar;
