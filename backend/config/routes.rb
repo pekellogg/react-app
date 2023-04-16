00ef256c1f56fca5324f6eb8b93e17271f818fe4
Peyton Kellogg <pekellogg@hotmail.com>
2023-04-16 15:32:19 -0700

    added snippet, 'get *path, to: fallback#index...' to narrow server resources visibility

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -6,4 +6,10 @@ Rails.application.routes.draw do
       resources :photos, only: [:index]
     end
   end
+  # this route definition matches:
+  # - *path: all paths not matched by one of the routes defined above
+  # - constraints:
+  #   - !req.xhr?: it's not a XHR (fetch) request
+  #   - req.format.html?: it's a request for a HTML document
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
