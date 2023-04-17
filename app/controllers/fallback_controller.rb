6debfc587db08d4d5605a2062afeadde94b2c149
Peyton Kellogg <pekellogg@hotmail.com>
2023-04-16 17:00:21 -0700

    added code comment for note to future self

--- app/controllers/fallback_controller.rb
+++ app/controllers/fallback_controller.rb
@@ -1,4 +1,5 @@
 class FallbackController < ActionController::Base
+  # inheriting from ActionController::Base for ability to render an HTML file for React
   def index
     render file: 'public/index.html'
   end
