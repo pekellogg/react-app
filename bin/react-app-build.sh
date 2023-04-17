1eebb1ea8dc6d916dd6d032400cd791279ad0678
Peyton Kellogg <pekellogg@hotmail.com>
2023-04-16 17:07:35 -0700

    added script to automate production build upon new changes

---
+++ bin/react-app-build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Add build commands for front end
+rm -rf public
+npm install --prefix client && npm run build --prefix client
+cp -a client/build/. public/
+
+# Add deps, migrate & seed rails db
+bundle install
+bundle exec rake db:migrate
+bundle exec rake db:seed
