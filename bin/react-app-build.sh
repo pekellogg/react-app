9c9225b86ea68cfcd72e9a39db4203f43d4166de
Peyton Kellogg <pekellogg@hotmail.com>
2023-04-18 15:30:24 -0700

    added bundle exec rake db:prepare before migrate & seed

--- bin/react-app-build.sh
+++ bin/react-app-build.sh
@@ -7,7 +7,8 @@ rm -rf public
 npm install --prefix client && npm run build --prefix client
 cp -a client/build/. public/
 
-# Add deps, migrate & seed rails db
+# Add deps, create if not yet created, migrate & seed rails db
 bundle install
+bundle exec rake db:prepare
 bundle exec rake db:migrate
 bundle exec rake db:seed
