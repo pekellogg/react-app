0ba7785c0fee7870b579a1ff98210a52801b1d54
Peyton Kellogg <pekellogg@hotmail.com>
2023-04-20 18:10:49 -0700

    added bundle exec rake db:reset to build script

--- bin/react-app-build.sh
+++ bin/react-app-build.sh
@@ -9,6 +9,4 @@ cp -a client/build/. public/
 
 # Add deps, create if not yet created, migrate & seed rails db
 bundle install
-bundle exec rake db:prepare
-bundle exec rake db:migrate
-bundle exec rake db:seed
+bundle exec rake db:reset
