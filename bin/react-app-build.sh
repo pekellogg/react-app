2b07735b911ae1b065664a7f2c7625e190a8906e
Peyton Kellogg <peytonkellogg@gmail.com>
2023-06-02 16:23:31 -0700

    Comments out Render-hosted build script's Rails database-related commands. Fixes 502 errors from visiting live site.

--- bin/react-app-build.sh
+++ bin/react-app-build.sh
@@ -11,7 +11,12 @@ cp -a client/build/. public/
 # Add deps, create db if not yet created, migrate, seed
 bundle install
 
-# Migrate the database
-bundle exec rake db:migrate
-# Loads the seed data from db/seeds.rb
-bundle exec rake db:seed
+# 1st, 1-time execution of build script in production
+  # Migrate the database
+  # bundle exec rake db:migrate
+
+  # Loads the seed data from db/seeds.rb
+  # bundle exec rake db:seed
+
+# all future executions of build script in production should not require migration + seeding rails commands
+  # see: "Production Site Reliability Troubleshooting" Trello card
