c1c6a3fea0c2c92050fb3ad791db54b14756eec8
Peyton Kellogg <peytonkellogg@gmail.com>
2023-06-16 12:51:20 -0700

    adds --color options to .rspec; removes Rails default test/* directory

--- test/controllers/action_items_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class ActionItemsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/controllers/action_plans_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class ActionPlansControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
