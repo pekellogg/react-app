bce201e526dba3bfaf01b9a96dd0fe8679fb059f
Peyton Kellogg <peytonkellogg@gmail.com>
2023-06-16 18:39:03 -0700

    tests camera's has_many :photos association

--- spec/models/camera_spec.rb
+++ spec/models/camera_spec.rb
@@ -1,5 +1,10 @@
-require 'rails_helper'
+require "rails_helper"
+require "camera"
 
 RSpec.describe Camera, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it "has_many :photos" do
+      is_expected.to have_many(:photos).class_name("Photo")
+    end
+  end
 end
