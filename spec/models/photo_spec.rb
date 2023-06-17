75d0fb1ee0d2ef7538d764aeab0d261f78c8d97d
Peyton Kellogg <peytonkellogg@gmail.com>
2023-06-16 17:43:04 -0700

    tests photo's belongs_to :rover association

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -3,6 +3,6 @@ require "photo"
 
 RSpec.describe Photo, type: :model do
   describe "associations" do
-    # it { should belong_to(:rover).class_name("Rover") }
+    it { should belong_to(:rover).class_name("Rover") }
   end
 end
