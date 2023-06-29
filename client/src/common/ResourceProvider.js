e35dba6ec52c778928ab4155b4a0130900dfbd5a
Peyton Kellogg <peytonkellogg@gmail.com>
2023-06-28 20:31:14 -0700

    refactors ResourceProvider to use 2 conditional returns and 1 return vs. switch statement

--- client/src/common/ResourceProvider.js
+++ client/src/common/ResourceProvider.js
@@ -2,40 +2,50 @@ import React from "react";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query"; // useMutation, useQueryClient
 
-export default function ResourceProvider({ children, resource, resourceContext: ResourceContext }) {
+export default function ResourceProvider({
+  children,
+  resource,
+  resourceContext: ResourceContext,
+}) {
   const resourceQuery = useQuery({
     queryKey: [resource],
     queryFn: fetchResource,
-  })
+  });
 
   function fetchResource() {
-    return (
-      axios
-        .get(resource)
-        .then((response) => response.data)
-    );
+    return axios.get(resource).then((response) => response.data);
   }
 
-  switch(resourceQuery) {
-    // display loading state
-    case resourceQuery.isLoading:
-      return (
-        <h1>Loading...</h1>
-      );
-    // display error state
-    case resourceQuery.isError:
-      return (
-        <pre>
-          { JSON.stringify(resourceQuery.error) }
-        </pre>
-      );
-    default:
-      return (
-        <>
-          <ResourceContext.Provider value={resourceQuery.data}>
-            { children }
-          </ResourceContext.Provider>
-        </>
-      );
-  }
+  // switch(resourceQuery) {
+  //   // display loading state
+  //   case resourceQuery.isLoading:
+  //     return (
+  //       <h1>Loading...</h1>
+  //     );
+  //   // display error state
+  //   case resourceQuery.isError:
+  //     return (
+  //       <pre>
+  //         { JSON.stringify(resourceQuery.error) }
+  //       </pre>
+  //     );
+  //   default:
+  //     return (
+  //       <>
+  //         <ResourceContext.Provider value={resourceQuery.data}>
+  //           { children }
+  //         </ResourceContext.Provider>
+  //       </>
+  //     );
+  // }
+  if (resourceQuery.isLoading) return <h1>Loading...</h1>;
+  if (resourceQuery.isError)
+    return <pre>{JSON.stringify(resourceQuery.error)}</pre>;
+  return (
+    <>
+      <ResourceContext.Provider value={resourceQuery.data}>
+        {children}
+      </ResourceContext.Provider>
+    </>
+  );
 }
