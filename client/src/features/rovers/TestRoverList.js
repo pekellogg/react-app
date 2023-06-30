16e5d72288b2322e73601c7a65a5cebdd834b5a2
Peyton Kellogg <peytonkellogg@gmail.com>
2023-06-30 14:39:04 -0700

    creates TestApp, TestRoverList, & testindex.js

--- client/src/features/rovers/TestRoverList.js
+++ client/src/features/rovers/TestRoverList.js
@@ -1,8 +1,58 @@
 import React from "react";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
+import { useRoverContext } from "./useRoverContext";
+import { useState } from "react";
+import RoverListItem from "./RoverListItem";
 
-export default function ResourceProvider({
+import "./RoverList.css";
+
+export default function RoverList() {
+  const rovers = useRoverContext();
+
+  // onClick toggle options:
+  // 1. RoverCard "index" view: all showing
+  // 2. Click RoverCard to reveal its cameras, hiding all other RoverCards
+  const handleClick = (e) => {
+    // investigate CameraButton 'wiring' to RoverList
+    if (e.target.attributes.class.value === "RoverCard") {
+      const roverId = e.target.attributes.roverid.value;
+      setLocalRovers((prevLocalRovers) =>
+        prevLocalRovers.map((r) => {
+          if (r.id.toString() === roverId) return { ...r };
+          if (r.style.display === "block") {
+            return { ...r, style: { display: "none" } };
+          } else {
+            return { ...r, style: { display: "block" } };
+          }
+        })
+      );
+    }
+  };
+
+  const [localRovers, setLocalRovers] = useState(
+    rovers.map((r) => ({
+      ...r,
+      onClick: handleClick,
+      style: { display: "block" },
+    }))
+  );
+
+  const displayRovers = () => {
+    return localRovers.map((r) => (
+      <RoverListItem
+        {...r}
+        key={r.id}
+        onClick={r.onClick}
+        style={r.style}
+      />
+    ));
+  };
+
+  return <div id="RoverList">{displayRovers()}</div>;
+}
+
+function ResourceProvider({
   children,
   resource,
   resourceContext: ResourceContext,
@@ -23,10 +73,7 @@ export default function ResourceProvider({
 
   return (
     <>
-      <ResourceContext.Provider value={resourceQuery.data}>
-        {children}
-        <pre>{JSON.stringify(resourceQuery.data)}</pre>
-      </ResourceContext.Provider>
+      <pre>{JSON.stringify(resourceQuery.data)}</pre>
     </>
   );
 }
--- client/src/testindex.js
+++ client/src/testindex.js
@@ -2,36 +2,22 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import ResourceProvider from "./common/ResourceProvider";
-import { cameraContext } from "./features/cameras/useCameraContext";
-import { photoContext } from "./features/photos/usePhotoContext";
-import { roverContext } from "./features/rovers/useRoverContext";
-import App from "./app/App";
+
+import TestApp from "./app/TestApp";
 
 const queryClient = new QueryClient();
 
 const domNode = document.getElementById("root");
 const root = createRoot(domNode);
 
+// "/api/v1/rovers"
+// "/api/v1/cameras"
+// "/api/v1/photos"
+
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ResourceProvider
-        resource="/api/v1/cameras"
-        resourceContext={cameraContext}
-      >
-        <ResourceProvider
-          resource="/api/v1/photos"
-          resourceContext={photoContext}
-        >
-          <ResourceProvider
-            resource="/api/v1/rovers"
-            resourceContext={roverContext}
-          >
-            <App />
-          </ResourceProvider>
-        </ResourceProvider>
-      </ResourceProvider>
+      <TestApp />
       <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
