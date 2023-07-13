ba87b055d0483b140b3f34aa136607fe78687111
Peyton <71993048+pekellogg@users.noreply.github.com>
2023-07-12 21:42:13 -0700

    Wip (#7)
    * removes usage of FetchResource in lieu of useFetch for rovers
    
    * removes react-query from deps in lieu of hooks-based useFetch
    
    * re-runs production build with script at bin/react-app-build.sh to preview production behavior

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
 import App from "./app/App";
-
-const queryClient = new QueryClient();
 
 const domNode = document.getElementById("root");
 const root = createRoot(domNode);
 
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>
 );
